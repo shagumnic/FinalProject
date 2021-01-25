91a83eccf561703abc943d042510b80e858c60e3
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-01-25 17:56:12 -0500

    basic app structure

--- server/app.py
+++ server/app.py
@@ -6,6 +6,26 @@ from build_db import *
 from csv import writer
 
 @app.route("/", methods=["GET", "POST"])
-def index():
-        return render_template("base.html")
+def main():
+        return redirect(url_for('index'))
 
+
+@app.route("/index", methods=["GET", "POST"])
+def index():
+        if request.method == "POST":
+                if request.form.get("add") == "Add":
+                        return redirect(url_for('add'))
+                if request.form.get("listAll") == "List all":
+                        return redirect(url_for('listAll'))
+
+        return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+        return render_template("add.html")
+
+
+@app.route("/list", methods=["GET", "POST"])
+def listAll():
+        return render_template("list.html")
