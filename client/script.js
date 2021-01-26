7f8bcb51b8c24a593eb0d86af45c91404c572ffe
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2021-01-26 17:38:43 -0500

    fixed duplicate function

--- client/script.js
+++ client/script.js
@@ -3,29 +3,6 @@
 
 const KEY_NAME = ['steam_id', 'name', 'release_date', 'languages', 'genres'];
 
-async function getVideoGameData() {
-
-    //let name = document.getElementById("videogame").value;
-
-    let videogame = await fetch(`http://127.0.0.1:5000/api/v1/?name=${name}`)
-        .then(response => response.json())
-        .catch(error => console.log(error));
-
-    let table = document.getElementById("tableId");
-
-    for (let game in videogame.results) {
-        let tr = document.createElement("tr");
-        
-        let td_id = document.createElement("td");
-        td_id.innerText = game.steam_id;
-        tr.appendChild(td_id);
-
-        let td_name = document.createElement("td");
-        td_name.innerText = game.name;
-        tr.appendChild(td_id);
-    }
-}
-
 async function fetchData() {
     let name = document.querySelector('#name').value;
     let data = await fetch(`http://127.0.0.1:5000/api/v1/?name=${name}`).then(response => response.json());
